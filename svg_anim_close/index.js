f3f46a3ff686220b89ada24a7804084565b13ad3
naklicoder <naklicoder@gmail.com>
2020-10-16 13:27:28 +0530

    update

--- svg_anim_close/index.js
+++
@@ -1,9 +0,0 @@
-var icon = document.getElementById("icon");
-
-icon.onclick = function() {
-  if (this.className === "Icon") {
-    this.className = "Icon open"; 
-  } else {
-    this.className = "Icon";
-  }
-};
